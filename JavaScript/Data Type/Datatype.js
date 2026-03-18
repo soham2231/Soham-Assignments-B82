369c62a34d9c89250a93a02fea4e2228f5775aa8
soham2231 <sohamcs2003@gmail.com>
2026-03-18 18:22:33 +0530

    Add files via upload

---
+++ JavaScript/Data Type/Datatype.js
@@ -0,0 +1,20 @@
+// let
+let color = "black";
+console.log("Before:", color);
+
+color = "red";
+console.log("After:", color);
+
+// var
+if (true) {
+  var message = "hello from inside block";
+}
+
+console.log(message);
+
+// const
+const fruits = ["apple", "banana", "mango"];
+
+fruits.push("orange");
+
+console.log(fruits);
